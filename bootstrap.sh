e1500521234a9024d42503d0238ac8e7dc6cb7c2
Dima Doroshev <dima@doroshev.com>
2021-04-13 10:06:36 +0300

    changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,11 @@
 set -ex
 
 # prepare
-sudo xcode-select --install || true
+# sudo xcode-select --install || true
 
 # brew and friends
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew bundle
+brew bundle -v
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
