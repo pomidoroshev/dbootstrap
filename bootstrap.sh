ea367e38614a7079a0902dbb9ffd2dacaef4830d
Dima Doroshev <dima@doroshev.com>
2021-04-14 18:47:45 +0300

    python

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,4 @@ set -ex
 brew -v || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle -v
 
-# python versions
-cat pyenvs | xargs -n 1 pyenv install -s
-
-# install poetry and ptipython for each pyenv
+./pyenv.sh
---
+++ pyenv.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# python versions
+cat pyenvs | xargs -n 1 pyenv install -s
+
+# install poetry and ptipython for each pyenv
+cat pyenvs | while read $pyver
+do
+   pyenv shell $pyver
+   pip install -r requirements.txt
+done
